c2c157f236f0987ff5001d5d73887a5e14548297
Andrew Herbig <notandrewherbig@gmail.com>
2020-12-03 12:55:53 -0500

    do some things just because

--- stats.js
+++ stats.js
@@ -1,98 +1,92 @@
-!function ($) {
-  'use strict';
-  
-  var FILE, LEGEND, ORDER, BALANCE_COLUMNS, HASHRATE_COLUMNS, options, rows
-  
-  FILE = 'week.json'
+!(function ($) {
+  "use strict";
 
-  LEGEND =
-  { immature: 'Immature Balance'
-  , unexchanged: 'Unexchanged Balance'
-  , balance: 'Balance'
-  , paid: 'Paid Out'
-  , accepted_rate: 'Accepted MH/s'
-  , rejected_rate: 'Rejected MH/s'
-  }
+  var FILE, LEGEND, ORDER, BALANCE_COLUMNS, HASHRATE_COLUMNS, options, rows;
 
-  ORDER = ['time', 'immature', 'unexchanged', 'balance', 'paid',
-           'accepted', 'rejected']
+  FILE = "week.json";
 
-  BALANCE_COLUMNS = ['paid', 'balance', 'unexchanged', 'immature']
+  LEGEND = {
+    immature: "Immature Balance",
+    unexchanged: "Unexchanged Balance",
+    balance: "Balance",
+    paid: "Paid Out",
+    accepted_rate: "Accepted MH/s",
+    rejected_rate: "Rejected MH/s",
+  };
 
-  HASHRATE_COLUMNS = ['rejected', 'accepted']
+  ORDER = [
+    "time",
+    "immature",
+    "unexchanged",
+    "balance",
+    "paid",
+    "accepted",
+    "rejected",
+  ];
 
-  options =
-  { series:
-    { stack: true
-    , lines:
-      { steps: true
-      , fill: true
-      }
-    }
-  , xaxis:
-    { mode: 'time'
-    , timeformat: '%b %e'
-    , minTickSize: [1, 'day']
-    }
-  , legend:
-    { position: 'nw'
-    }
-  }
+  BALANCE_COLUMNS = ["paid", "balance", "unexchanged", "immature"];
+
+  HASHRATE_COLUMNS = ["rejected", "accepted"];
+
+  options = {
+    series: { stack: true, lines: { steps: true, fill: true } },
+    xaxis: { mode: "time", timeformat: "%b %e", minTickSize: [1, "day"] },
+    legend: { position: "nw" },
+  };
 
   $.getJSON(FILE, function (data) {
-    var row, i, len
-    
+    var row, i, len;
+
     for (i = 0, len = data.length; i < len; ++i) {
-      row = data[i]
-      row[0] = row[0] * 1000
+      row = data[i];
+      row[0] = row[0] * 1000;
     }
-    
-    rows = data
-    
-    draw()
-  })
+
+    rows = data;
+
+    draw();
+  });
 
   function draw() {
-    var column, i, series, len
-    
-    series = []
-    
+    var column, i, series, len;
+
+    series = [];
+
     for (i = 0, len = BALANCE_COLUMNS.length; i < len; ++i) {
       column = BALANCE_COLUMNS[i];
-      if ($(':checkbox[name="' + column + '"]').is(':checked')) {
+      if ($(':checkbox[name="' + column + '"]').is(":checked")) {
         series.push({
-          label: LEGEND[column]
-        , data: rows.map(function (row) {
+          label: LEGEND[column],
+          data: rows.map(function (row) {
             return [row[0], row[ORDER.indexOf(column)]];
-          })
-        , color: i
-        , lines:
-          { fill: column !== 'paid'
-          }
-        })
+          }),
+          color: i,
+          lines: { fill: column !== "paid" },
+        });
       }
     }
-    
-    $.plot('#balance', series, options)
-    
-    series = []
-    
+
+    $.plot("#balance", series, options);
+
+    series = [];
+
     for (i = 0, len = HASHRATE_COLUMNS.length; i < len; ++i) {
-      column = HASHRATE_COLUMNS[i]
-      
-      series.push(
-      { label: LEGEND[column]
-      , data: rows.filter(function (row) {
-          return typeof row[ORDER.indexOf(column)] === 'number'
-        })
-        .map(function (row) {
-          return [row[0], row[ORDER.indexOf(column)]]
-        })
-      })
+      column = HASHRATE_COLUMNS[i];
+
+      series.push({
+        label: LEGEND[column],
+        data: rows
+          .filter(function (row) {
+            return typeof row[ORDER.indexOf(column)] === "number";
+          })
+          .map(function (row) {
+            return [row[0], row[ORDER.indexOf(column)]];
+          }),
+      });
     }
-    
-    $.plot('#hashrate', series, options)
+
+    $.plot("#hashrate", series, options);
   }
 
-  $(':checkbox').click(draw)
-}(window.jQuery)
+  $(":checkbox").click(draw);
+})(window.jQuery);
--- stats.py
+++ stats.py
@@ -1,60 +1,75 @@
-import requests
-
-import os.path
 import json
-from time import strptime
+import os.path
 from calendar import timegm
 from datetime import timedelta
+from time import strptime
+
+import requests
 
 dirname = os.path.dirname(os.path.abspath(__file__))
 
-with open(os.path.join(dirname, 'config.json'), 'r') as f:
-  cfg = json.load(f)
+with open(os.path.join(dirname, "config.json"), "r") as f:
+    cfg = json.load(f)
 
-TOTAL_ORDER = ['totalImmatureBalance', 'totalUnexchangedBalance',
-               'totalBalance', 'totalPaidOut', 'totalMegahashesPerSecond',
-               'totalRejectedMegahashesPerSecond']
-USERS_ORDER = ['immatureBalance', 'unexchangedBalance', 'bitcoinBalance',
-               'paidOut', 'megahashesPerSecond', 'rejectedMegahashesPerSecond',
-               'lastHourShares', 'lastHourRejectedShares']
+TOTAL_ORDER = [
+    "totalImmatureBalance",
+    "totalUnexchangedBalance",
+    "totalBalance",
+    "totalPaidOut",
+    "totalMegahashesPerSecond",
+    "totalRejectedMegahashesPerSecond",
+]
+USERS_ORDER = [
+    "immatureBalance",
+    "unexchangedBalance",
+    "bitcoinBalance",
+    "paidOut",
+    "megahashesPerSecond",
+    "rejectedMegahashesPerSecond",
+    "lastHourShares",
+    "lastHourRejectedShares",
+]
 
-TOTAL = object() # sentinel
+TOTAL = object()  # sentinel
+
 
 def convert(x):
-  x = float(x)
-  if x % 1 == 0:
-    x = int(x)
-  return x
+    x = float(x)
+    if x % 1 == 0:
+        x = int(x)
+    return x
+
 
 def write(address, timestamp, report):
-  if address is TOTAL:
-    path = cfg['total']
-    order = TOTAL_ORDER
-  else:
-    path = os.path.join(cfg['users'], address + '.json')
-    order = USERS_ORDER
-  if os.path.exists(path):
-    with open(path, 'r') as f:
-      history = json.load(f)
-  else:
-    history = []
-  output = [convert(report.get(key, 0)) for key in order]
-  if not history or history[-1][1:] != output:
-    history.append([timestamp] + output)
-    with open(path, 'w') as f:
-      json.dump(history, f, separators=(',', ':'))
     if address is TOTAL:
-      week = []
-      maxdelta = timedelta(days=7)
-      for row in history:
-        if timedelta(seconds=(timestamp - row[0])) < maxdelta:
-          week.append(row)
-      with open(cfg['week'], 'w') as f:
-        json.dump(week, f, separators=(',', ':'))
+        path = cfg["total"]
+        order = TOTAL_ORDER
+    else:
+        path = os.path.join(cfg["users"], address + ".json")
+        order = USERS_ORDER
+    if os.path.exists(path):
+        with open(path, "r") as f:
+            history = json.load(f)
+    else:
+        history = []
+    output = [convert(report.get(key, 0)) for key in order]
+    if not history or history[-1][1:] != output:
+        history.append([timestamp] + output)
+        with open(path, "w") as f:
+            json.dump(history, f, separators=(",", ":"))
+        if address is TOTAL:
+            week = []
+            maxdelta = timedelta(days=7)
+            for row in history:
+                if timedelta(seconds=(timestamp - row[0])) < maxdelta:
+                    week.append(row)
+            with open(cfg["week"], "w") as f:
+                json.dump(week, f, separators=(",", ":"))
 
-data = requests.get('http://middlecoin.com/json', timeout=10).json()
-timestamp = timegm(strptime(data['time'], '%Y-%m-%d %H:%M:%S'))
+
+data = requests.get("http://middlecoin.com/json", timeout=10).json()
+timestamp = timegm(strptime(data["time"], "%Y-%m-%d %H:%M:%S"))
 
 write(TOTAL, timestamp, data)
-# for address, rep in data['report']:
-#   write(address, timestamp, rep)
+for address, rep in data["report"]:
+    write(address, timestamp, rep)
